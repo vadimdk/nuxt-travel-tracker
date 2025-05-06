98bce85c6bff5e6967bc119f5b267a13a3761da7
vadimdk <vadim.dk81@gmail.com>
2025-05-06 17:59:31 +0300

    :sparkles: feat: add Tailwind CSS and DaisyUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+
   modules: ["@nuxt/eslint"],
+  css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 });
