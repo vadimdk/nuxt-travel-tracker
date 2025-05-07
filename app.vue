e4a978e61c0021db3cf6a7f6d02aed75ae39be90
vadimdk <vadim.dk81@gmail.com>
2025-05-07 13:34:43 +0300

    ✨ feat: add Tailwind CSS / DaisyUI / icons (#2)
    * add ESLint configuration and pre-commit hook;
    
    * :sparkles: feat: add Tailwind CSS and DaisyUI
    
    * :sparkles: add @nuxt/icon module

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-accent">
+      View Map <Icon name="tabler:map" />
+    </button>
   </div>
 </template>
