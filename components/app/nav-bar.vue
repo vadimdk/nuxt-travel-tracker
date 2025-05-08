d2d8e2baab547005a40f9856118c2bf89b2b4466
vadimdk <vadim.dk81@gmail.com>
2025-05-08 17:29:14 +0300

    ✨ feat: add layout and add navbar

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Tracker
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-accent">Sign In <Icon name="tabler:brand-google" size="24" /></a>
+    </div>
+  </div>
+</template>
